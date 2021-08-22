32ada87cccfc9474d4f415dcfc3dcc42068b19ce
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-22 17:21:18 +0530

    check if a number is even or odd

---
+++ src/com/bridgeLabz/coreJava/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.bridgeLabz.coreJava;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		int number=25;
+		if(number%2==0) {
+			System.out.println(number +" is even");
+		}
+		else {
+			System.out.println(number +" is odd");
+		}
+
+	}
+
+}
