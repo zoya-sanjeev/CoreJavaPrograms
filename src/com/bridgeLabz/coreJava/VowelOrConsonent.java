c04fd936dd9335ff4d88578357bbaedf3f2d8351
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-22 17:26:59 +0530

    check if alphabet is vowel or consonant

---
+++ src/com/bridgeLabz/coreJava/VowelOrConsonent.java
@@ -0,0 +1,17 @@
+package com.bridgeLabz.coreJava;
+
+public class VowelOrConsonent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char alphabet='B';
+		if(alphabet=='a' ||alphabet=='e' ||alphabet=='i' ||alphabet=='o' ||alphabet=='u' ||alphabet=='A' ||alphabet=='E' ||alphabet=='I' ||alphabet=='O' ||alphabet=='U') {
+			System.out.println("The alphabet is a vowel");
+		}
+		else {
+			System.out.println("The alphabet is not a vowel");
+		}
+
+	}
+
+}
