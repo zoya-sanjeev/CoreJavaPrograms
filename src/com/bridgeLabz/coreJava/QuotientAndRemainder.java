417d9c193cdab28c251afd865ee0e7f66cb61007
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-22 17:09:24 +0530

    computation of quotient and remainder

---
+++ src/com/bridgeLabz/coreJava/QuotientAndRemainder.java
@@ -0,0 +1,14 @@
+package com.bridgeLabz.coreJava;
+
+public class QuotientAndRemainder {
+
+	public static void main(String[] args) {
+		int dividend=38;
+		int divisor=3;
+		int quotient, remainder;
+		System.out.println("Quotient="+dividend/divisor);
+		System.out.println("Remainder="+dividend%divisor);
+
+	}
+
+}
